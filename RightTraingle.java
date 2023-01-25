2d44d2cfc67fda134e2ffc56bf8d2e30dc6ed7b8
Waseem Ahmed <82138949+waseem2k11@users.noreply.github.com>
2023-01-26 00:21:59 +0500

    Add files via upload

---
+++ RightTraingle.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class RightTraingle {
+
+	public static void main(String[] args) {
+
+		for (int a = 1; a <= 10; a++) {
+
+			for (int b = 1; b <= a; b++) {
+
+				System.out.print("*");
+
+			}
+			System.out.println("");
+		}
+	}
+}

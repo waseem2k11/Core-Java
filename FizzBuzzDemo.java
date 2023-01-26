cde1023ff909647885ace8ab40cb6f430c420077
Waseem Ahmed <82138949+waseem2k11@users.noreply.github.com>
2023-01-27 00:24:49 +0500

    Update FizzBuzzDemo.java

--- FizzBuzzDemo.java
+++ FizzBuzzDemo.java
@@ -1,3 +1,14 @@
+/*
+* Title: FizzBuzz (Core Java Logical Program)
+* Author: Waseem Ahmed
+* Description:
+*     Program that prints the numbers from 1 to n. But for multiples of
+*     3 print "Fizz" instead of the number and for the multiples of five
+*     print "Buzz". For numbers which are multiples of both three and five
+*     print "FizzBuzz". For numbers which are neither multiplies of 3 and nor 5, print that number
+*     and each result will be printed on new line.
+*/
+
 package com.test;
 
 import java.util.Scanner;
@@ -5,15 +16,10 @@ import java.util.Scanner;
 public class FizzBuzzDemo {
 
 	public static void main(String[] args) {
-
 		Scanner input = new Scanner(System.in);
-
 		int n = input.nextInt();
-
 		FizzBuzzDemo.fizzBuzz(n);
-
 		input.close();
-
 	}
 
 	private static void fizzBuzz(int n) {
